2287238e1bbdad099d704e65a1bf530711cc89cc
Girish-1-Goyal <lucifersdecp@gmil.com>
2024-12-22 16:46:44 +0530

    Template for cp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+/*
+*
+*    Author : Girish Kumar Goyal.
+*
+*/
+
+#include <bits/stdc++.h>
+#include "debug_utils.h"
+
+using namespace std;
+
+void solve(){
+    //Code Here....
+}
+
+void init_code(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){
+    init_code();
+    solve();
+    return 0;
+}
