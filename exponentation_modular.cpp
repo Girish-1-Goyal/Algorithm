94c11736360be82c399d647e666bc8a546749274
Girish-1-Goyal <lucifersdecp@gmil.com>
2024-12-29 17:17:32 +0530

    Exponentation

---
+++ exponentation_modular.cpp
@@ -0,0 +1,76 @@
+/*
+*
+*    Author : Girish Kumar Goyal.
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < class c > struct rge {
+    c b, e;
+};
+template < class c > rge<c> range(c i, c j){
+    return rge<c>{i, j};
+}
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+    ~debug() { cerr << endl; }
+    template < class c > typename enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+        cerr << boolalpha << i;
+        return * this;
+    }
+    template < class c > typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+        return * this << range(begin(i), end(i)); 
+    }
+    template < class c, class b > debug & operator << (pair < b, c > d) {
+        return * this << "(" << d.first << ", " << d.second << ")";
+    }
+    template < class c > debug & operator <<(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        return * this << "]";
+    }
+};  
+#define imie(...) " [" << #__VA_ARGS__ " : " << (__VA_ARGS__) << "]"
+
+#define MOD 1000000007
+
+long long expo(long long a, long long b){
+    long long result = 1;
+    while(b){
+        if(b & 1){
+            result = (result * a) % MOD;
+        }
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+void solve(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long a, b;
+        cin >> a >> b;
+        long long res = expo(a, b);
+        cout << res << "\n";
+    }
+}
+
+void init_code(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){
+    init_code();
+    solve();
+    return 0;
+}
