f53dcab0b3abbf490027ee371243e85617d65b4d
Girish-1-Goyal <lucifersdecp@gmil.com>
2025-01-12 13:52:36 +0530

    Count set bits

---
+++ count_set_bits.cpp
@@ -0,0 +1,65 @@
+/*
+*
+*    Author : Girish Kumar Goyal.
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < class c > struct rge {
+    c b, e;
+};
+template < class c > rge<c> range(c i, c j){
+    return rge<c>{i, j};
+}
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+    ~debug() { cerr << endl; }
+    template < class c > typename enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+        cerr << boolalpha << i;
+        return * this;
+    }
+    template < class c > typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+        return * this << range(begin(i), end(i)); 
+    }
+    template < class c, class b > debug & operator << (pair < b, c > d) {
+        return * this << "(" << d.first << ", " << d.second << ")";
+    }
+    template < class c > debug & operator <<(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        return * this << "]";
+    }
+};  
+#define imie(...) " [" << #__VA_ARGS__ " : " << (__VA_ARGS__) << "]"
+
+//count the no of setbits in the number of binary
+//this algorithm is called Brian Kernighan's Algorithms
+
+void solve(){
+    int n;
+    cin >> n;
+    unsigned int cnt = 0;
+    while(n != 0){
+        n &= (n - 1);
+        cnt++;
+    }
+    cout << cnt << "\n";
+}
+
+void init_code(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){
+    init_code();
+    solve();
+    return 0;
+}
