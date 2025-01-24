4b295230616238475ece973434a70b28efc3c1b5
Girish-1-Goyal <lucifersdecp@gmil.com>
2025-01-24 20:33:52 +0530

    Binary search in 2D matrix

---
+++ binarySearch2D.cpp
@@ -0,0 +1,79 @@
+/*
+*
+*    Author : Girish Kumar Goyal.
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < class c > struct rge {
+    c b, e;
+};
+template < class c > rge<c> range(c i, c j){
+    return rge<c>{i, j};
+}
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+    ~debug() { cerr << endl; }
+    template < class c > typename enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+        cerr << boolalpha << i;
+        return * this;
+    }
+    template < class c > typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+        return * this << range(begin(i), end(i)); 
+    }
+    template < class c, class b > debug & operator << (pair < b, c > d) {
+        return * this << "(" << d.first << ", " << d.second << ")";
+    }
+    template < class c > debug & operator <<(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        return * this << "]";
+    }
+};  
+#define imie(...) " [" << #__VA_ARGS__ " : " << (__VA_ARGS__) << "]"
+
+// implement a binary search in a 2D array
+
+bool binarySearch2D(vector<vector<int>> &arr, int target){
+    int n = arr.size();
+    int m = arr[0].size();
+    int low = 0;
+    int high = n * m - 1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        int row = mid / m;
+        int col = mid % m;
+        if(arr[row][col] == target){
+            return true;
+        }
+        else if(arr[row][col] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return false;
+}
+
+void solve(){
+    //Code Here....
+}
+
+void init_code(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){
+    init_code();
+    solve();
+    return 0;
+}
